12a3af018461a43f4b774f9136e54e040e574d1a
Mehmet Kaan Özkan <kozkan5624@gmail.com>
2024-10-14 23:10:25 +0200

    add: animated sprites for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,87 @@
 using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
     private Rigidbody2D rb;
-    
+    private SpriteRenderer sr;
+
+    public Sprite[] forwardSprites;
+    public Sprite[] sidewaysSprites;
+    public Sprite[] backwardSprites;
+    public float animationSpeed = 0.3f;
+    public float moveSpeed = 5.0f;
+    private int currentFrame;
+    private float timer;
+   
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        currentFrame = 0;
+        timer = 0;
     }
     
     // Update is called once per frame
     void Update()
     {
         Move();
+        Timer();
     }
 
     private void Move()
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
-
+        
         Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        if (moveDirection != Vector2.zero)
+        {
+            animationSpeed = 0.1f;
+            if (moveDirection.x == 0)
+            {
+                if (moveDirection.y > 0)
+                {
+                    sr.sprite = backwardSprites[currentFrame];
+                }
+                else
+                {
+                    sr.sprite = forwardSprites[currentFrame];
+                }
+            }
+            else
+            {
+                sr.sprite = sidewaysSprites[currentFrame];
+                if (moveDirection.x < 0)
+                {
+                    sr.flipX = false;
+                }
+                else
+                {
+                    sr.flipX = true;
+                }
+            }
+        }
+        else
+        {
+            animationSpeed = 0.2f;
+            sr.sprite = forwardSprites[currentFrame];
+        }
+    }
+    private void Timer()
+    {
+        timer += Time.deltaTime;
+        if (timer >= animationSpeed)
+        {
+            currentFrame++;
+            if (currentFrame >= 5)
+            {
+                currentFrame = 0;
+            }
+            timer = 0f;
+        }
     }
 }
