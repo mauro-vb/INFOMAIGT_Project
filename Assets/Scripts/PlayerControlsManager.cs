105c260ff5bd7625e35942bdc7a552741b862f68
Mauro <mvb.codes@gmail.com>
2024-10-24 16:34:43 +0200

    improved lvl7

--- Assets/Scripts/PlayerControlsManager.cs
+++ Assets/Scripts/PlayerControlsManager.cs
@@ -10,7 +10,7 @@ public class PlayerControlsManager : MonoBehaviour
 
     void Start()
     {
-        FindLargestResourceController();
+        //FindLargestResourceController();
     }
 
     public void RegisterResourceController(ResourceController resourceController)
