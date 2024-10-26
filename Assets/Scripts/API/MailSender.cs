43be5b267108d17f9f8bdae4f13f51c8afcdf4bb
Vlad Puscaru <vlad.cpuscaru@gmail.com>
2024-10-26 12:31:32 +0200

    Use MailJet C# client to send email to avoid CORS policy issues

--- Assets/Scripts/API/MailSender.cs
+++ Assets/Scripts/API/MailSender.cs
@@ -1,17 +1,29 @@
-using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Mailjet.Client;
+using Mailjet.Client.TransactionalEmails;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class MailSender
 {
+    /* Using corsproxy for bypassing CORS without handling a separate server */
     private static readonly string BASE_URL = "https://api.mailjet.com/v3.1";
     private static readonly string API_KEY = "9c085655c76f76a4cca85e565d4023a6"; /* really secure haha */
     private static readonly string SECRET_KEY = "041e0fee9a00dec54e54e9c3fd539d8d"; /* really secure haha */
 
     private static readonly bool SANDBOX_MODE = false;
+
+    private MailjetClient client;
+    private TransactionalEmail email;
+
+    public MailSender()
+    {
+        client = new MailjetClient(
+            API_KEY,
+            SECRET_KEY);
+    }
 
     // https://dev.mailjet.com/email/guides/send-api-v31/#sandbox-mode
     // {
@@ -64,37 +76,65 @@ public class MailSender
         public bool SandboxMode;
     }
 
-    // Function to send the POST request
+    public async Task<bool> SendRequest()
+    {
+        var response = await client.SendTransactionalEmailAsync(email);
+        return response.Messages.Length == 1;
+    }
+
     public IEnumerator SendEmail(BodyData body, System.Action<bool> onComplete)
     {
         var url = BASE_URL + "/send"; // Replace with your API URL
         UnityWebRequest request = new UnityWebRequest(url, "POST");
 
         body.SandboxMode = SANDBOX_MODE;
-        string bodyJson = JsonUtility.ToJson(body);
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJson);
-        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = new DownloadHandlerBuffer();
 
-        request.SetRequestHeader("Content-Type", "application/json");
-        
-        /* Basic Auth user:pass base64 encoded */
-        var userPass = System.Text.Encoding.UTF8.GetBytes(API_KEY + ":" + SECRET_KEY);
-        var auth = System.Convert.ToBase64String(userPass);
-        request.SetRequestHeader("Authorization", "Basic " + auth);
+        // construct your email with builder
+        email = new TransactionalEmailBuilder()
+            .WithFrom(new SendContact("vlad.cpuscaru@gmail.com"))
+            .WithSubject("INFOMAIGT Game Answer")
+            .WithHtmlPart(body.Messages[0].HTMLPart)
+            .WithTo(new SendContact("vlad.cpuscaru@gmail.com"))
+            .Build();
 
-        yield return request.SendWebRequest();
+        // invoke API to send email
+        var t = Task.Run(SendRequest);
+        yield return new WaitUntil(() => t.IsCompleted);
 
-        if (request.result == UnityWebRequest.Result.ConnectionError ||
-            request.result == UnityWebRequest.Result.ProtocolError)
+        if (t.Result)
         {
-            Debug.Log("Error: " + request.downloadHandler.text);
-            onComplete(false);
+            onComplete(true);
         }
         else
         {
-            Debug.Log("Response: " + request.downloadHandler.text);
-            onComplete(true);
+            onComplete(false);
         }
+
+        //
+        // string bodyJson = JsonUtility.ToJson(body);
+        // byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJson);
+        // request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        // request.downloadHandler = new DownloadHandlerBuffer();
+        //
+        // request.SetRequestHeader("Content-Type", "application/json");
+        //
+        // /* Basic Auth user:pass base64 encoded */
+        // var userPass = System.Text.Encoding.UTF8.GetBytes(API_KEY + ":" + SECRET_KEY);
+        // var auth = System.Convert.ToBase64String(userPass);
+        // request.SetRequestHeader("Authorization", "Basic " + auth);
+        //
+        // yield return request.SendWebRequest();
+        //
+        // if (request.result == UnityWebRequest.Result.ConnectionError ||
+        //     request.result == UnityWebRequest.Result.ProtocolError)
+        // {
+        //     Debug.Log("Error: " + request.downloadHandler.text);
+        //     onComplete(false);
+        // }
+        // else
+        // {
+        //     Debug.Log("Response: " + request.downloadHandler.text);
+        //     onComplete(true);
+        // }
     }
 }
